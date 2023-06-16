4c9222ea9ed4bf1f4d9ded30e648d7d8822529a4
alex <o.lototskyi@gmail.com>
2023-06-16 12:28:41 +0300

    store loader in the component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader} from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -39,16 +39,7 @@ const router = createBrowserRouter([
                 path: "events",
                 element: <EventsRootLayout />,
                 children: [
-                    { index: true, element: <EventsPage />, loader: async () => {
-                        const response = await fetch("http://localhost:8080/events");
-
-                        if (!response.ok) {
-
-                        } else {
-                            const resData = await response.json();
-                            return resData.events;
-                        }
-                    } },
+                    { index: true, element: <EventsPage />, loader: eventsLoader},
                     { path: ":eventId", element: <EventDetailPage /> },
                     { path: "new", element: <NewEventPage /> },
                     {
