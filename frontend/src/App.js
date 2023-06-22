5062ac04bc293139afe8f9dab4b36aa99ab254ca
alex <o.lototskyi@gmail.com>
2023-06-22 12:22:52 +0300

    error handling in React router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,11 +28,13 @@ import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
+        errorElement: <ErrorPage />,
         children: [
             { index: true, element: <HomePage /> },
             {
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -15,7 +15,10 @@ export async function loader() {
     const response = await fetch("http://localhost:8080/events");
 
     if (!response.ok) {
-
+        throw new Response(
+            JSON.stringify({ message: "Could not fetch events." }),
+            { status: 500 }
+        );
     } else {
         return response;
     }
