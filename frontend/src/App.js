23a946586f4ecde02b450525ac1daa276f0175c6
alex <o.lototskyi@gmail.com>
2023-06-22 15:00:17 +0300

    loader for event page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader} from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, { loader as eventDetailLoader} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
                 element: <EventsRootLayout />,
                 children: [
                     { index: true, element: <EventsPage />, loader: eventsLoader},
-                    { path: ":eventId", element: <EventDetailPage /> },
+                    { path: ":eventId", element: <EventDetailPage />, loader: eventDetailLoader },
                     { path: "new", element: <NewEventPage /> },
                     {
                         path: ":eventId/edit",
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,15 +1,23 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
 
-    const params = useParams();
+    const data = useLoaderData();
 
     return (
-        <>
-            <h1>EventDetailPage</h1>
-            <p>Event ID: {params.eventId}</p>
-        </>
+        <EventItem event={data.event} />
     );
 };
 
 export default EventDetailPage;
+
+export async function loader({request, params}) {
+    const response = await fetch('http://localhost:8080/events/' + params.eventId);
+    
+    if (!response.ok) {
+        throw json({message: 'Could not fetch details for selected event.'}, {status: 500});
+    } else {
+        return response;
+    }
+}
