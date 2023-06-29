ddea55af74bbf1bc1d7a13d17e7c16b6f7c9e5f3
alex <o.lototskyi@gmail.com>
2023-06-29 17:42:00 +0300

    useNavigation to get status, showing submitting process

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,13 @@
-import { Form, useNavigate } from "react-router-dom";
+import { Form, useNavigate, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
     const navigate = useNavigate();
+    const navigation = useNavigation();
+
+    const isSubmitting = navigation.state === 'submitting';
+
     function cancelHandler() {
         navigate("..");
     }
@@ -51,10 +55,10 @@ function EventForm({ method, event }) {
                 />
             </p>
             <div className={classes.actions}>
-                <button type="button" onClick={cancelHandler}>
+                <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
                     Cancel
                 </button>
-                <button>Save</button>
+                <button disabled={isSubmitting}>{isSubmitting ? 'Submitting' : 'Save'}</button>
             </div>
         </Form>
     );
