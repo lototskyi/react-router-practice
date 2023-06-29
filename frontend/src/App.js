a1ea474f22b6379ad03c0f11d35ac6f5879039d7
alex <o.lototskyi@gmail.com>
2023-06-29 16:07:57 +0300

    adding a new event by using react router action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
     loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
                         ],
                     },
 
-                    { path: "new", element: <NewEventPage /> },
+                    { path: "new", element: <NewEventPage />, action: newEventAction },
                 ],
             },
         ],
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 const NewEventPage = () => {
@@ -5,3 +6,28 @@ const NewEventPage = () => {
 };
 
 export default NewEventPage;
+
+export async function action({request, params}) {
+    const data = await request.formData();
+
+    const eventData = {
+        title: data.get('title'),
+        image: data.get('image'),
+        date: data.get('date'),
+        description: data.get('description')
+    };
+
+    const response = await fetch('http://localhost:8080/events', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(eventData)
+    });
+
+    if (!response.ok) {
+        throw json({message: 'Could not save event.'}, {status: 500});
+    }
+
+    return redirect('/events');
+}
