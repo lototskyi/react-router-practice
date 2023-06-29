8c3e923779ebe9d6a27b3acadcffe4551acf86b0
alex <o.lototskyi@gmail.com>
2023-06-29 16:26:32 +0300

    action nfor deleteng an event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,7 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
     loader as eventDetailLoader,
+    action as deleteEventAction
 } from "./pages/EventDetail";
 import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -56,6 +57,7 @@ const router = createBrowserRouter([
                             {
                                 index: true,
                                 element: <EventDetailPage />,
+                                action: deleteEventAction
                             },
                             {
                                 path: "edit",
