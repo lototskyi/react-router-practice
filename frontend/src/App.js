01ad995c5cb5d1ad6685948968f7a6ef81b77d37
alex <o.lototskyi@gmail.com>
2023-07-08 16:46:43 +0300

    using useFetcher for newsletter form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,6 +33,7 @@ import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
     {
@@ -74,6 +75,11 @@ const router = createBrowserRouter([
                         action: manipulateEventAction,
                     },
                 ],
+            },
+            {
+                path: "newsletter",
+                element: <NewsletterPage />,
+                action: newsletterAction,
             },
         ],
     },
--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
+import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
     return (
@@ -27,8 +28,19 @@ function MainNavigation() {
                             Events
                         </NavLink>
                     </li>
+                    <li>
+                        <NavLink
+                            to="/newsletter"
+                            className={({ isActive }) =>
+                                isActive ? classes.active : undefined
+                            }
+                        >
+                            Newsletter
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
+            <NewsletterSignup />
         </header>
     );
 }
