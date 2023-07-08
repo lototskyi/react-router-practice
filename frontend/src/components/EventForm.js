97ae39f8c08fe824582442527b75486f81d4e9e7
alex <o.lototskyi@gmail.com>
2023-07-08 13:31:35 +0300

    validation form and using useActionData

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,19 +1,32 @@
-import { Form, useNavigate, useNavigation } from "react-router-dom";
+import {
+    Form,
+    useNavigate,
+    useNavigation,
+    useActionData
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+    const data = useActionData();
     const navigate = useNavigate();
     const navigation = useNavigation();
 
-    const isSubmitting = navigation.state === 'submitting';
+    const isSubmitting = navigation.state === "submitting";
 
     function cancelHandler() {
         navigate("..");
     }
 
     return (
-        <Form method='post' className={classes.form}>
+        <Form method="post" className={classes.form}>
+            {data && data.errors && (
+                <ul>
+                    {Object.values(data.errors).map((err) => (
+                        <li key={err}>{err}</li>
+                    ))}
+                </ul>
+            )}
             <p>
                 <label htmlFor="title">Title</label>
                 <input
@@ -55,10 +68,16 @@ function EventForm({ method, event }) {
                 />
             </p>
             <div className={classes.actions}>
-                <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
+                <button
+                    type="button"
+                    onClick={cancelHandler}
+                    disabled={isSubmitting}
+                >
                     Cancel
                 </button>
-                <button disabled={isSubmitting}>{isSubmitting ? 'Submitting' : 'Save'}</button>
+                <button disabled={isSubmitting}>
+                    {isSubmitting ? "Submitting" : "Save"}
+                </button>
             </div>
         </Form>
     );
